3a52d935a7d6334fd4c21a03d29302151a4789ab
mac <mac@local>
2023-10-08 12:09:26 -0600

    Updated Project Structure

---
+++ src/days/day01.rs
@@ -0,0 +1,77 @@
+use std;
+
+// ACCII for the different symbols
+const CARRAIGE_RETURN: u8 = 13;
+static NEWLINE: u8 = 10;
+
+// did think about it but went back to the "CULT OF DONE" mentality
+// struct Elve {
+//     calories: Vec<i32>,
+// }
+
+pub fn day01answers() {
+    let filedata = std::fs::read("data/day01.txt").unwrap();
+    // need to have the carraige return and newline character in acii
+    let mut staging_vector = Vec::new();
+    // need a placeholder vector to hold the numbers
+    let mut elve_calories: Vec<i32> = Vec::new();
+    let mut elve_total: Vec<i32> = Vec::new();
+
+    for i in 0..filedata.len() {
+        //
+
+        // file data is ACCII still need to take bytes and convert them to ints
+
+        if filedata[i] == CARRAIGE_RETURN {
+            // skip the carraiage return character
+            continue;
+        }
+
+        if filedata[i] == NEWLINE {
+            // newline signifies that it has moved on to the next item
+            //println!("{:?}",staging_vector);
+            // need to compress the vec to a single int
+            //println!("{:?}",staging_vector.iter().collect::<String>() as i32);
+
+            let calorie_placeholder = staging_vector.iter().collect::<String>(); // my first use of the turbo fish!
+
+            if calorie_placeholder.len() > 0 {
+                // skips over ones with no values
+                let calorie_value = calorie_placeholder.parse::<i32>().unwrap(); // convert the string to i32
+                                                                                 //println!("{:?}", calorie_value);
+                elve_calories.push(calorie_value);
+            }
+
+            if staging_vector.len() == 0 {
+                // length of zero signifies that there was two newlines back to back meaning there values are for a new elve
+                //println!("new elve");
+                // sum the elve_vector
+                elve_total.push(elve_calories.iter().sum());
+                elve_calories.clear();
+            }
+            // clear staging
+            staging_vector.clear();
+            // continue aka skip this "Newline" index
+            continue;
+        }
+
+        // print!("{:?} ", filedata[i]);                           // no change
+        // print!("{:?} ", i8::from_be_bytes([filedata[i]; 1]));   // no change
+        // print!("{:?} ", filedata[i]as char);                    // works at converting the u8 accii to number
+        staging_vector.push(filedata[i] as char); //.try_into().expect("miss match"));
+                                                  //print!("{:?}",staging_vector);
+    }
+    // if not newline or carraige return then populate vector
+    // if newline
+    let max_calories = elve_total.iter().max().unwrap();
+    println!(
+        "Day 01 Part 01: Number of elves {:?}, elve {:?} has the most calories at {:?}",
+        elve_total.len(),
+        elve_total
+            .iter()
+            .position(|element| element == max_calories) // syntax is very clean, can use it for the next step in problem one
+            .unwrap(),
+        max_calories
+    );
+    //let 
+}
---
+++ src/days/mod.rs
@@ -0,0 +1,2 @@
+// this file acts as a way of routing the data contained in the sub folder to main.rs
+pub mod day01;
